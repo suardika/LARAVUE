5884d163c595b09f91c36f9e154f7e84ba4dc5fc
staff <1234@worldbinaryexchange.com>
2016-10-27 15:05:54 +0800

    first commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,13 @@
+// var Vue = require('vue/dist/vue.js');
+ 
+// new Vue({
+//     el: '#app',
+ 
+//     data: {message: 'Hello World!'},
+ 
+//     filters: {
+//         reverse: require('./filters/reverse')
+//     }
+ 
+// })
+
