a9fd9ec592ca376e45a2fcb2d7145cf4f82c64bd
suardika <agratitudesign@gmail.com>
2016-10-30 23:34:59 +0800

    stuck binding property vue2

--- public/js/app.js
+++ public/js/app.js
@@ -7520,16 +7520,267 @@ return Vue$3;
 
 var Vue = require('vue/dist/vue.js');
 
-Vue.component('counter', {
-	template: '#counter-template',
-	data: function data() {
-		return { count: 0 };
-	}
+Vue.component('tasks', {
+	props: ['list'],
+	template: '#tasks-template'
 });
 
 new Vue({
-	el: '#app'
+	el: '#app',
+	data: {
+		tasks: [{ body: 'Go to the store', completed: false }, { body: 'Go to the bank', completed: false }, { body: 'Go to the doctor', completed: true }]
+	}
 });
+
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		tasks: [
+// 			{ body: 'Go to the store', completed: false },
+// 			{ body: 'Go to the bank', completed: false },
+// 			{ body: 'Go to the doctor', completed: true }
+// 		]
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		tasks: [
+// 			{ body: 'Go to the store', completed: false },
+// 			{ body: 'Go to the bank', completed: false },
+// 			{ body: 'Go to the doctor', completed: true }
+// 		]
+// 	},
+// 	methods: {
+// 		toggleCompletedFor: function(task) {
+// 			task.completed = ! task.completed;
+// 		}
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		tasks: [
+// 			'Go to the store',
+// 			'Go to the bank',
+// 			'Go to the doctor',
+// 		]
+// 	}
+// }); 
+
+
+// var bus = new Vue();
+// Vue.component('plan', {
+//     template: '#plan-template',
+//     props: ['plan', 'active'],
+
+//     computed: {
+//         isActive: function() {
+//             return this.plan.price === this.active.price;
+//         },
+
+//         isUpgrade: function() {
+//             return this.plan.price > this.active.price;
+//         }
+//     },
+
+//     methods: {
+//         setActivePlan: function() {
+//             bus.$emit('activate', this.plan);
+//         }
+//     }
+// });
+// new Vue({
+//     el: '#app',
+//     data: {
+//         plans: [
+//             { name: 'Enterprise', price: 100},
+//             { name: 'Pro', price: 50},
+//             { name: 'Personal', price: 10},
+//             { name: 'Free', price: 0}
+//         ],
+//         active: { name: 'Free', price: 0}
+//     },
+//     methods: {
+//         handleActivation: function(plan) {
+//             this.active = plan;
+//             console.log(plan);
+//         }
+//     },
+//     created: function() {
+//         bus.$on('activate', this.handleActivation);
+//     },
+//     destroyed: function() {
+//         bus.$off('activate', this.handleActivation);
+//     }
+// });
+
+
+// depricated
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		plans: [
+// 			{ name: 'Enterprice', price: 100 },
+// 			{ name: 'Pro', price: 50 },
+// 			{ name: 'Personal', price: 10 }, 
+// 			{ name: 'Free', price: 0 }
+// 		],
+// 		active: {}
+// 	},
+// 	components: {
+// 		plan: {
+// 			template: '#plan-template',
+// 			props: ['plan', 'active'],
+
+// 			methods: {
+// 				setActivePlan: function() {
+// 					this.active = this.plan;
+// 				}
+// 			}
+// 		}
+// 	}
+// });
+
+
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		plans: [
+// 			{ name: 'Enterprice', price: 100 },
+// 			{ name: 'Pro', price: 50 },
+// 			{ name: 'Personal', price: 10 }, 
+// 			{ name: 'Free', price: 0 }
+// 		]
+// 	},
+// 	components: {
+// 		plan: {
+// 			template: '#plan-template',
+// 			props: ['plan'],
+// 			data: function() {
+// 				return {
+// 					active: false
+// 				};
+// 			},
+// 			methods: {
+// 				setActivePlan: function() {
+// 					this.active = this.plan;
+// 				}
+// 			}
+// 		}
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '.container',
+// 	data: {
+// 		first: 'Jeffrey',
+// 		last: 'Way'
+// 	},
+// 	computed: {
+// 		fullname: function() {
+// 			return this.first + ' ' + this.last;
+// 		}
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '.container',
+// 	data: {
+// 		first: 'Jeffrey',
+// 		last: 'Way',
+// 		fullname: 'Jeffrey Way'
+// 	},
+// 	watch: {
+// 		first: function(first) {
+// 			// alert(first);
+// 			this.fullname = first + ' ' + this.last;
+// 		},
+// 		last: function(last) {
+// 			this.fullname = this.first + ' ' + last;
+// 		}
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '.container',
+// 	data: {
+// 		points:50,
+// 	},
+// 	computed: {
+// 		skill: function() {
+// 			if (this.points <= 100) {
+// 				return 'Beginner';
+// 			}
+// 			return 'Advanced';
+// 		}
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '.container',
+// 	data: {
+// 		points:300,
+// 	},
+// 	computed: {
+// 		skill: function() {
+// 			return 'Advanced';
+// 		}
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '.container',
+// 	data: {
+// 		points:300,
+// 		skill: '',
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '.container',
+// 	data: {
+// 		skill: 'Beginner',
+// 	}
+// }); 
+
+
+// new Vue({
+// 	el: '#app',
+// 	components: {
+// 		counter: {
+// 			template:'#counter-template',
+// 			props:['subject'],
+// 			data: function(){
+// 				return {count:0}; 
+// 			}
+// 		}
+// 	}
+// }); 
+
+
+// Vue.component('counter', {
+// 	template:'#counter-template',
+// 	props:['subject'],
+// 	data: function(){
+// 		return {count:0}; 
+// 	}
+// });
+
+// new Vue({
+// 	el: '#app',
+// });
+
 
 // Vue.component('counter', {
 // 	template:'<h1>Hellow  World!</h1><button>'
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,22 +24,147 @@
                 transform: translate(-50%, -50%);
             }
 
+            .plan__name, .plan__price {
+                float: left;
+                width: 10%;
+            }
+            #app div {
+                margin-top: 10px;
+                margin-bottom: 10px;
+            }
+            .completed { text-decoration: line-through; }
+
         </style>
 
     </head>
     <body>
 
-    <div id="app">
-        <counter></counter>
-        <counter></counter>
-    </div> 
+        <div id="app" class="container">
+            <tasks :list="tasks"></tasks>
+            <tasks :list="tasks"></tasks>
+            <tasks :list="[{ body: 'do this', completed: false }]"></tasks>
+        </div>
 
+        <template id="tasks-template">
+            <ul>
+                <li :class="{'completed': task.completed }"
+                    v-for="task in list"
+                    @click="task.completed = ! task.completed"
+                >
+                    @{{ task.body }}
+                </li>
+            </ul>            
+        </template>
+
+ <!--        <div id="app" class="container">
+            <ul>
+                <li :class="{'completed': task.completed }"
+                    v-for="task in tasks"
+                    @click="task.completed = ! task.completed"
+                >
+                    @{{ task.body }}
+                </li>
+            </ul>
+        </div>     -->
+
+<!--         <div id="app" class="container">
+            <ul>
+                <li :class="{'completed': task.completed }"
+                    v-for="task in tasks"
+                    @click="toggleCompletedFor(task)"
+                >
+                    @{{ task.body }}
+                </li>
+            </ul>
+        </div>     -->    
+
+
+<!--         <div id="app" class="container">
+            <ul>
+                <li v-for="task in tasks">@{{ task }}</li>
+            </ul>
+        </div> -->
+
+
+<!--         <div id="app" class="container">
+            <pre>
+                @{{ $data }}
+            </pre>
+            <div v-for="plan in plans">
+                <plan :plan="plan" :active="active" @activate="handleActivation"></plan>
+            </div>
+        </div>
+
+        <template id="plan-template">
+            <div>
+                <span class="plan__name">
+                    @{{ plan.name }}
+                </span>
+                <span class="plan__price">
+                    @{{ plan.price }}/month
+                </span>
+                <button class="btn btn-default" v-if="!isActive" @click="setActivePlan">
+                    @{{ isUpgrade ? 'Upgrade' : 'Downgrade' }}
+                </button>
+                <button class="btn btn-primary" v-else>Selected</button>
+            </div>
+        </template> -->
+
+
+<!--         <div id="app" class="container">
+            <div v-for="plan in plans">
+                <plan :plan="plan"></plan>                
+            </div>
+        </div>
+
+        <template id="plan-template">
+            <div>
+                @{{ $data }}
+                <span class="plan__name">
+                    @{{ plan.name }}
+                </span>
+                <span class="plan__price">
+                    @{{ plan.price }}/month
+                </span>
+                <button @click="setActivePlan">
+                    Upgrade
+                </button>
+            </div>
+        </template>  -->
+
+<!--         <div class="container">
+             <h1>@{{ fullname }}</h1>
+             <input type="text" v-model="first" placeholder="First Name">
+             <input type="text" v-model="last" placeholder="Last Name">
+        </div>     -->
+
+<!--         <div class="container">
+             <h1>@{{ first }} @{{ last }}</h1>
+        </div>     -->         
+
+<!--         <div class="container">
+            <h1>Skill: @{{ skill }}</h1>
+            <input type="text" v-model="points">
+        </div>              
+ -->
+<!--         <div class="container">
+            <h1>Skill: @{{ points <= 100 ? 'Beginner' : 'Intermediate'}}</h1>
+        </div>      -->   
+
+<!--         <div class="container">
+            <h1>Skill: @{{ skill }}</h1>
+        </div> -->
+
+<!--     <div id="app">
+        <counter subject="Likes"></counter>
+        <counter subject="Dislikes"></counter>
+    </div> 
     <template id="counter-template">
         <section>
-            <h1>Heading</h1> 
-            <button @click="count += 1">Increment: @{{ count }}</button>
+            <h1>@{{ subject }}</h1> 
+            <button @click="count += 1">@{{ count }}</button>
         </section>
-     </template>
+     </template> -->
 
 <!--     <div id="app">
         <counter></counter>
